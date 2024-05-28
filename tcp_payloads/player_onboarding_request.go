dc2e6d331a11a5966584991c87da9def2cb38d33
chriswolf <chriswolfdesign@gmail.com>
2024-05-28 07:28:13 -0400

    Adds payload types to payloads

--- tcp_payloads/player_onboarding_request.go
+++ tcp_payloads/player_onboarding_request.go
@@ -1,5 +1,15 @@
 package tcp_payloads
 
+import "github.com/chriswolfdesign/tcp-ttt-common/strings"
+
 type PlayerOnboardingRequest struct {
 	Name string
+	PayloadType string
+}
+
+func GeneratePlayerOnboardingRequest(name string) PlayerOnboardingRequest {
+	return PlayerOnboardingRequest{
+		Name: name,
+		PayloadType: strings.TYPE_ONBOARDING_REQUEST,
+	}
 }
